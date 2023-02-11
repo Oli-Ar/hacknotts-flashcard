a0427f536d6a2c2e117f25694bd97fec17410c06
Oliver A <oliver@archer.fi>
2023-02-11 12:08:46 +0000

    Added interface to flask backend.
    Updated App.js and npm modules proxy to interface with the python flask
    backend

--- flashcards-react/src/App.js
+++ flashcards-react/src/App.js
@@ -1,13 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from "react"
 
 function App() {
+    const [data, setdata] = useState({
+        name: "",
+        age: 0,
+        date: "",
+        programming: "",
+    });
+
+    useEffect(() => {
+      // Using fetch to fetch the api from 
+      // flask server it will be redirected to proxy
+      fetch("/data").then((res) =>
+          res.json().then((data) => {
+              // Setting a data from api
+              setdata({
+                test: data.test
+              });
+          })
+      );
+    }, []);
+  
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload. {data.test}
         </p>
         <a
           className="App-link"
