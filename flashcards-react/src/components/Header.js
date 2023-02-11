e9c0fc9066c0c3194e8814dbf83a876d6d00456f
ArtyBr <artemiy.brukhno@gmail.com>
2023-02-11 14:21:34 +0000

    Added components for Header and Flashcard.

---
+++ flashcards-react/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Header.css'
+
+export default function NavBar () {
+    return (
+        <nav className="container">
+            <h1 className="nav--titleText underline font-bold text-3xl">
+                Flashcards App
+            </h1>
+        </nav>
+    )
+}
