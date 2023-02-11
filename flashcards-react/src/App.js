27c85ae50502cdb021ba5cdf003a074381e52e8f
ArtyBr <artemiy.brukhno@gmail.com>
2023-02-11 14:22:51 +0000

    Added css files for components

--- flashcards-react/src/App.js
+++ flashcards-react/src/App.js
@@ -1,6 +1,6 @@
-import logo from './logo.svg';
 import './App.css';
 import React, { useState, useEffect } from "react"
+import Header from "./components/Header.js"
 
 function App() {
     const [data, setdata] = useState({
@@ -26,18 +26,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. {data.test}
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Header /> {data.test}
       </header>
     </div>
   );
