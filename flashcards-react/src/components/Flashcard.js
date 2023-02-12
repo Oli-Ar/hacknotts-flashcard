0b12c742ffd09c65fecae9e4477d6272083c0457
Oliver A <oliver@archer.fi>
2023-02-12 03:41:33 +0000

    Added audio to flashcards

--- flashcards-react/src/components/Flashcard.js
+++ flashcards-react/src/components/Flashcard.js
@@ -11,10 +11,16 @@ function Flashcard(props) {
                 <div className="cardTitleText questionText">{props.card.englishContent}</div>
                 <div className="hr"> </div>
             </button>
-            <button className="card card--back" onClick={() => setFlip(!flip)}>
+            <button className="card card--back"  onClick={() => setFlip(!flip)}>
                 <div className="cardTitleText answerText">{props.card.languageContent}</div>
                 <div className="hr"></div>
-                
+                <button className="audio" onClick={e => {
+                    e.stopPropagation();
+                    const audio = new Audio(props.card.phoneticAudioLink);
+                    audio.play();
+                }}>
+                    <img className="audioImage" src="https://www.svgrepo.com/show/141310/audio.svg" alt="audio" />
+                </button>
                 <img className="cardImage" src={props.card.imageLink} alt="Sample" />
             </button>
         </ReactCardFlip>
