5511ec118055ba6c0b8a2b6ead31dc3d97e8cfd7
ArtyBr <artemiy.brukhno@gmail.com>
2023-02-12 02:47:13 +0000

    NextCard feature implemented fully very cool

--- flashcards-react/src/App.js
+++ flashcards-react/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React, { useState, useEffect } from "react"
+import React, { useState, useEffect, useRef } from "react"
 import Navbar from "./components/Navbar"
 import Flashcard from "./components/Flashcard"
 
@@ -8,14 +8,34 @@ function App() {
     const [currentCard, setCurrentCard] = useState({})
     const [nextCard, setNextCard] = useState({})
 
+    const [flip, setFlip] = useState(false)
+
+    const allCardIdsRef = useRef([]);
+
+    function handleFlip() {
+      setFlip(!flip)
+    }
+
+    function sleep(ms) { 
+      return new Promise(resolve => setTimeout(resolve, ms));
+    }
+
+    async function handleNext() {
+      if (flip){
+        setFlip(false);
+        await sleep(500);
+      }
+      setCurrentCard(nextCard)
+      getCard(allCardIds, +localStorage.getItem("currentCard")+1)
+    }
+
     useEffect(() => {
-      // Using fetch to fetch the api from 
-      // flask server it will be redirected to proxy
       fetch("/data?lang=es").then(res => {
           if (res.ok) {
             res.json().then(idList => {
+              allCardIdsRef.current = idList;
               setAllCardIds(idList);
-              getCard(idList);
+              getCard(idList, idList.length);
             }).catch(e => console.error(e));
           } else {
             console.error(res.statusText);
@@ -23,9 +43,11 @@ function App() {
       });
     }, []);
 
-    const getCard = cards => {
-      [[0, setCurrentCard], [1, setNextCard]].forEach(a => {
-        fetch(`/data?id=${cards[a[0]]}`).then(res => { 
+    const getCard = (cards, i) => {
+      console.log(i % cards.length);
+      localStorage.setItem("currentCard", i);
+      [[i, setCurrentCard], [i+1, setNextCard]].forEach(a => {
+        fetch(`/data?id=${cards[a[0] % allCardIdsRef.current.length]}`).then(res => { 
           if (res.ok) {
             res.json().then(idObj => a[1](idObj)).catch(e => console.error(e));
           } else {
@@ -39,8 +61,8 @@ function App() {
     <div className="App">
         <Navbar className="Nav" />
         <div className="mainContent">
-          <Flashcard className="Flashcard" card={currentCard}/>
-          <button className="nextButton">
+          <Flashcard className="Flashcard" card={currentCard} flip={flip} handleFlip={handleFlip}/>
+          <button className="nextButton" onClick={handleNext/*()=>getCard(allCardIds, +localStorage.getItem("currentCard")+1)*/}>
             <div className="buttonText">
               Next Card
             </div>
--- flashcards-react/src/components/Flashcard.js
+++ flashcards-react/src/components/Flashcard.js
@@ -1,17 +1,16 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ReactCardFlip from "react-card-flip";
 import "./Flashcard.css"
   
 function Flashcard(props) {
-    const [flip, setFlip] = useState(false);
     return (
-        <ReactCardFlip isFlipped={flip} 
+        <ReactCardFlip isFlipped={props.flip} 
             flipDirection="horizontal">
-            <button className="card card--front"  onClick={() => setFlip(!flip)}>
+            <button className="card card--front"  onClick={props.handleFlip}>
                 <div className="cardTitleText questionText">{props.card.englishContent}</div>
                 <div className="hr"> </div>
             </button>
-            <button className="card card--back" onClick={() => setFlip(!flip)}>
+            <button className="card card--back" onClick={props.handleFlip}>
                 <div className="cardTitleText answerText">{props.card.languageContent}</div>
                 <div className="hr"></div>
                 
